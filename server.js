9a511cfa2517c72a2dc3b23b69e840c03e4004e1
Devin K Yuan <dkyuan1@gmail.com>
2025-04-15 16:07:54 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// Home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+app.get("/climate-crisis", (req, res) => {
+    res.send(`<h1>Climate Crisis</h1><a href="https://github.com/BinaryFallow/Climate-Crisis" target="_blank">View Assignment Repo</a>`);
+  });
+app.get("/typesetting", (req, res) => {
+    res.send(`<h1>Typesetting</h1><a href="https://github.com/BinaryFallow/typesetting" target="_blank">View Assignment Repo</a>`);
+  });
+app.get("/four-algorithms", (req, res) => {
+    res.send(`<h1>Four Algorithms</h1><a href="https://github.com/BinaryFallow/four-algorithms" target="_blank">View Assignment Repo</a>`);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
